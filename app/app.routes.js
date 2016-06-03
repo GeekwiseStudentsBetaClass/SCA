45ba6a1e8afad884cd1430340bfb5d1275d0e6ec
Joseph Halderman <josephhalderman@Josephs-MacBook-Pro-2.local>
2016-06-02 20:33:18 -0700

    created base to go off of

---
+++ app/app.routes.js
@@ -0,0 +1,51 @@
+angular.module('app')
+    .config(function($stateProvider,$urlRouterProvider){
+        // ROUTER PROVIDER ASSIGNS THE DEFAULT URL PAGE
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            // ASSIGNING STATES
+            .state('home',{
+                url:'/home',
+                templateUrl:'home/home.html',
+                controller:"homeCtrl"
+            })
+            // .state('',{
+            //     url:'/',
+            //     templateUrl:''
+            // })
+            // CREATING A CHILD STATE
+            .state('home.facebook',{
+              url:'/facebook',
+              templateUrl:'tabs/facebook.html',
+              controller:'homeCtrl'
+            })
+
+            .state('home.twitter',{
+              url:'/twitter',
+              templateUrl:'tabs/twitter.html',
+              controller:'homeCtrl'
+            })
+
+            .state('home.weather',{
+              url:'/weather',
+              templateUrl:'tabs/weather.html',
+              controller:'homeCtrl'
+            })
+
+            .state('home.comments',{
+              url:'/comments',
+              templateUrl:'tabs/comments.html',
+              controller:'homeCtrl'
+            })
+
+            .state('home.profile',{
+              url:'/profile',
+              templateUrl:'tabs/profile.html',
+              controller:'homeCtrl'
+            })
+
+            .state('home.contact',{
+                url:'/contact',
+                templateUrl:'tabs/contact.html'
+            });
+    });
